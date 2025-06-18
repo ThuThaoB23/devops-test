a14d5a4126433cdd1fe40f9d2dae138517acc157
thuctd <thuc@gmail.com>
2025-06-18 13:55:38 +0700

    fibonacy

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello world devops");
+function generateFibonacci(n) {
+    const fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+    return fib.slice(0, n);
+}
+
+// Example: Generate the first 100 Fibonacci numbers
+const first100Fibonacci = generateFibonacci(100);
+console.log(first100Fibonacci);
